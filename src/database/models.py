3af21fc91f09f38343b17a0b123d6bdba96abf5f
Denys <denkuryshko@gmail.com>
2024-04-17 13:11:31 +0300

    add Authorization and Authentication

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,19 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
 
 
 class Contact(Base):
@@ -14,6 +26,8 @@ class Contact(Base):
     phone_number = Column(String)
     birthday = Column(Date)
     additional_data = Column(String)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="contacts")
 
     def __repr__(self):
         return f"<Contact(first_name='{self.first_name}', last_name='{self.last_name}', email='{self.email}', phone_number='{self.phone_number}', birthday='{self.birthday}', additional_data='{self.additional_data}')>"
--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from pydantic import BaseModel
-from datetime import date
+from pydantic import BaseModel, Field
+from datetime import date, datetime
 
 
 class ContactBase(BaseModel):
@@ -10,6 +10,34 @@ class ContactBase(BaseModel):
     phone_number: str
     birthday: date
     additional_data: Optional[str] = None
+
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: str
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+    avatar: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
 
 
 class ContactCreate(ContactBase):
